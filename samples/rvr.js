9d767fb33277a0f9241cd8f996a157489124b1f7
ricardovrodri <93518567+ricardovrodri@users.noreply.github.com>
2023-03-11 21:19:35 +0100

    Posts

--- samples/rvr.js
+++ samples/rvr.js
@@ -11,7 +11,9 @@ var csvdata = require('csvdata');
 
 module.exports = (app) => {
 
-    // LoadInitialData
+//___________________________________GETS__________________________________
+
+    // GET LoadInitialData
     app.get(`${BASE_API_URL_ss_affiliates}/loadInitialData`, (req, res) => {
         console.log(`New Request to /loadInitialData.`);
         db.find({}, async (err, data) => {
@@ -30,7 +32,7 @@ module.exports = (app) => {
         });
     });
 
-    //GET Lista Recursos
+    //GET /ss-affiliates: Lista de Recursos
     app.get(`${BASE_API_URL_ss_affiliates}`, (req, res) => {
         let year = req.query.year;
         let from = req.query.from;
@@ -39,7 +41,7 @@ module.exports = (app) => {
         if(year != undefined){
             //Get por fecha
             console.log('Nueva peticion por fecha');
-            db.find({'year' : parseInt(year)}, (err, data) => {
+            db.find({'year' : parseInt(year)}, {_id : 0}, (err, data) => {
                 if(err){
                     console.log(`Error getting ss-affiliates?year=${year}: ${err}`);
                     res.sendStatus(500);
@@ -48,16 +50,13 @@ module.exports = (app) => {
                     res.sendStatus(404);
                 }else{
                     console.log(`Data of ss-affiliates?year=${year} returned: ${data.length}`);
-                    res.json(data.map((c) => {
-                        delete c._id;
-                        return(c);
-                    }));
+                    res.json(data);
                 }
             });
         }else if(from !== undefined && to !== undefined){
             //Get por Rango de fechas
             console.log('Nueva peticion por rango de fecha');
-            db.find({'year' : {$gte: parseInt(from), $lte: parseInt(to)}}, (err, data) => {
+            db.find({'year' : {$gte: parseInt(from), $lte: parseInt(to)}}, {_id : 0}, (err, data) => {
                 if(err){
                     console.log(`Error getting ss-affiliates?from=${from}&to=${to}: ${err}`);
                     res.sendStatus(500);
@@ -66,15 +65,12 @@ module.exports = (app) => {
                     res.sendStatus(404);
                 }else{
                     console.log(`Data of ss-affiliates?from=${from}&to=${to} returned: ${data.length}`);
-                    res.json(data.map((c) => {
-                        delete c._id;
-                        return(c);
-                    }));
+                    res.json(data);
                 }
             });
         }else{
             //Get sin query
-            db.find({}, (err, data) => {
+            db.find({}, {_id : 0},  (err, data) => {
                 if(err){
                     console.log(`Error getting ss-affiliates: ${err}`);
                     res.sendStatus(500);
@@ -83,10 +79,7 @@ module.exports = (app) => {
                     res.sendStatus(404);
                 }else{
                     console.log(`Data returned: ${data.length}`);
-                    res.json(data.map((c) => {
-                        delete c._id;
-                        return(c);
-                    }));
+                    res.json(data);
                 }
             });
         }
@@ -94,12 +87,12 @@ module.exports = (app) => {
     });
 
 
-    //GET Recurso (First Province, Second Year)
+    //GET /ss-affiliates/province/year (First Province, then Year): Recurso único
     app.get(`${BASE_API_URL_ss_affiliates}/:province/:year`, (req, res) => {
         let year = req.params.year;
         let province = req.params.province;
         console.log(`New request to /ss-affiliates/${province}/${year}`);
-        db.find({'year': parseInt(year), 'province' : province}, (err, data) => {
+        db.find({'year': parseInt(year), 'province' : province}, {_id : 0}, (err, data) => {
             if(err){
                 console.log(`Error getting ss-affiliates/${province}/${year}: ${err}`);
                 res.sendStatus(500);
@@ -108,15 +101,12 @@ module.exports = (app) => {
                 res.sendStatus(404);
             }else{
                 console.log(`Data ss-affiliates/${province}/${year} returned`);
-                res.json(data.map((c) => {
-                    delete c._id;
-                    return(c);
-                }));
+                res.json(data);
             }
         });
     });
 
-    //Get Recursos por provincia
+    //GET /ss-affiliates/province: Recursos por provincia
     app.get(`${BASE_API_URL_ss_affiliates}/:province`, (req, res) => {
         let province = req.params.province;
         let from = req.query.from;
@@ -124,7 +114,7 @@ module.exports = (app) => {
         console.log(`New request to /ss-affiliates/${province}`);
         if(from !== undefined && to !== undefined){
             //Get por provincia y rango de fechas
-            db.find({'province' : province, year : {$gte: parseInt(from), $lte: parseInt(to)}}, (err, data) => {
+            db.find({'province' : province, year : {$gte: parseInt(from), $lte: parseInt(to)}}, {_id : 0}, (err, data) => {
                 if(err){
                     console.log(`Error getting ss-affiliates/${province}?from=${from}&to=${to}: ${err}`);
                     res.sendStatus(500);
@@ -133,15 +123,12 @@ module.exports = (app) => {
                     res.sendStatus(404);
                 }else{
                     console.log(`Data of ss-affiliates/${province}?from=${from}&to=${to} returned: ${data.length}`);
-                    res.json(data.map((c) => {
-                        delete c._id;
-                        return(c);
-                    }));
+                    res.json(data);
                 }
             });
         }else if(from !== undefined){
             // Get por provincia y fecha de inicio
-            db.find({'province' : province, year : {$gte: parseInt(from)}}, (err, data) => {
+            db.find({'province' : province, year : {$gte: parseInt(from)}}, {_id : 0}, (err, data) => {
                 if(err){
                     console.log(`Error getting ss-affiliates/${province}?from=${from}: ${err}`);
                     res.sendStatus(500);
@@ -150,15 +137,12 @@ module.exports = (app) => {
                     res.sendStatus(404);
                 }else{
                     console.log(`Data of ss-affiliates/${province}?from=${from} returned: ${data.length}`);
-                    res.json(data.map((c) => {
-                        delete c._id;
-                        return(c);
-                    }));
+                    res.json(data);
                 }
             });
         }else if(to !== undefined){
             // Get por provincia y fecha de fin
-            db.find({'province' : province, year : {$lte: parseInt(to)}}, (err, data) => {
+            db.find({'province' : province, year : {$lte: parseInt(to)}}, {_id : 0}, (err, data) => {
                 if(err){
                     console.log(`Error getting ss-affiliates/${province}?to=${to}: ${err}`);
                     res.sendStatus(500);
@@ -167,15 +151,12 @@ module.exports = (app) => {
                     res.sendStatus(404);
                 }else{
                     console.log(`Data of ss-affiliates/${province}?to=${to} returned: ${data.length}`);
-                    res.json(data.map((c) => {
-                        delete c._id;
-                        return(c);
-                    }));
+                    res.json(data);
                 }
             });
         }else{
             // Get por provincia
-            db.find({'province' : province}, (err, data) => {
+            db.find({'province' : province}, {_id : 0} , (err, data) => {
                 if(err){
                     console.log(`Error getting ss-affiliates/${province}: ${err}`);
                     res.sendStatus(500);
@@ -184,14 +165,106 @@ module.exports = (app) => {
                     res.sendStatus(404);
                 }else{
                     console.log(`Data of ss-affiliates/${province} returned: ${data.length}`);
-                    res.json(data.map((c) => {
-                        delete c._id;
-                        return(c);
-                    }));
+                    res.json(data);
                 }
             });
         }
     });
+
+
+//___________________________POSTS_____________________________________________
+
+
+    //POST /ss-affiliates : Crear recurso
+    app.post(`${BASE_API_URL_ss_affiliates}`, (req, res) => {
+
+        //Guardamos el body de la request
+        let newRecurse = req.body;
+
+        //Comprobamos si tiene todos los campos
+        if(!newRecurse.hasOwnProperty('province') || 
+        !newRecurse.hasOwnProperty('year') || 
+        !newRecurse.hasOwnProperty('ss_afiliation') ||
+        !newRecurse.hasOwnProperty('n_cont_indef') || 
+        !newRecurse.hasOwnProperty('n_cont_eventual') ||
+        !newRecurse.hasOwnProperty('n_cont_temporary')){
+
+            //Avisamos de que falta algun campo
+            console.log('Falta algun dato');
+            res.sendStatus(400);
+        
+        //Todos los campos están presentes
+        }else{
+
+            //Vamos a crear solo si no existe, para ellos buscamos el recurso
+            db.find({'year': parseInt(newRecurse.year), 'province' : newRecurse.province}, (err, data) => {
+
+                if(err){
+
+                    //Error al buscar si existe dicho recurso, avisamos
+                    console.log(`Error getting ss-affiliates/${newRecurse.province}/${newRecurse.year}: ${err}`);
+                    res.sendStatus(500);
+
+                //No hay errores, crear si no existe:
+                }else if(data.length == 0){
+
+                    //Modificamos el tipo de los valores al correcto
+                    newRecurse.year = parseInt(newRecurse.year);
+                    newRecurse.ss_afiliation = parseInt(newRecurse.ss_afiliation);
+                    newRecurse.n_cont_indef = parseInt(newRecurse.n_cont_indef);
+                    newRecurse.n_cont_temporary = parseInt(newRecurse.n_cont_temporary);
+                    newRecurse.n_cont_eventual = parseInt(newRecurse.n_cont_eventual);
+
+                    //Guardamos el nuevo dato
+                    db.insert(newRecurse);
+
+                    //Avisamos a consola y a usuario
+                    console.log(`Created ${BASE_API_URL_ss_affiliates}/${newRecurse.province}/${newRecurse.year}`);
+                    res.sendStatus(201);
+
+                //Data.length no vacío, ya existe el recurso
+                }else{
+
+                    //El recurso a crear ya existe, avisamos
+                    console.log(`Data ss-affiliates/${newRecurse.province}/${newRecurse.year} already exist`);
+                    res.sendStatus(409);
+                }
+            });
+        }
+    });
+
+
+    //POST NO PERMITIDO /ss-affiliates/province/year
+    app.post(`${BASE_API_URL_ss_affiliates}/:province/:year`, (req, res) => {
+        console.log('Metodo no permitido');
+        res.sendStatus(405);
+    });
+
+    //POST NO PERMITIDO /ss-affiliates/province
+    app.post(`${BASE_API_URL_ss_affiliates}/:province`, (req, res) => {
+        console.log('Metodo no permitido');
+        res.sendStatus(405);
+    });
+
+
+//_________________________________DELETES________________________________
+
+    app.delete(`${BASE_API_URL_ss_affiliates}/:province/:year`, (req, res) => {
+        let province = req.params.province;
+        let year = req.params.year;
+
+        db.remove({'province': province, 'year': parseInt(year)}, {}, (err, num) => {
+            if(err){
+                console.log(`Error ocurred removing ${BASE_API_URL_ss_affiliates}/${province}/${year}`);
+                res.sendStatus(500);
+            }else{
+                console.log(``)
+            }
+        })
+    });
+
+
+
 
 
     // Samples
