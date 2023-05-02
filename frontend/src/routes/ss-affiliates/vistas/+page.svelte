0978245d847de7ee9f7939ef8224342c9527b5af
ricardovrodri <93518567+ricardovrodri@users.noreply.github.com>
2023-05-03 00:30:42 +0200

    Vista Afiliados 2

--- frontend/src/routes/ss-affiliates/vistas/+page.svelte
+++ frontend/src/routes/ss-affiliates/vistas/+page.svelte
@@ -15,9 +15,37 @@
 
     onMount(async () => {
 
-        let affiliates = await getAffiliation();
+        const affiliation = await getAffiliation();
 
-        if (affiliates.length === 0 ){
+        // @ts-ignore
+        const sevilla2 = affiliation.filter((affiliate) => affiliate.province==='sevilla').sort(function(a, b) {return a.year - b.year;}).map((affiliate)=> {return {ss_affiliation: affiliate.ss_affiliation, n_cont_indef:affiliate.n_cont_indef, n_cont_eventual: affiliate.n_cont_eventual, n_cont_temporary:affiliate.n_cont_temporary }})
+        
+        let sevilla_affiliation = ['data1']
+
+        // @ts-ignore
+        sevilla_affiliation = sevilla_affiliation.concat(sevilla2.map(affiliate => affiliate.ss_affiliation))
+
+        const data = sevilla_affiliation
+
+        let sevilla_eventual = ['data2']
+
+        // @ts-ignore
+        sevilla_eventual = sevilla_eventual.concat(sevilla2.map(affiliate => affiliate.n_cont_eventual))
+
+        const data2 = sevilla_eventual
+
+        let sevilla_temporary = ['data3']
+
+        // @ts-ignore
+        sevilla_temporary = sevilla_temporary.concat(sevilla2.map(affiliate => affiliate.n_cont_temporary))
+
+        const data3 = sevilla_temporary
+
+        console.log(sevilla_affiliation)
+        console.log(sevilla_eventual)
+        console.log(sevilla_temporary)
+
+        if (affiliation.length === 0 ){
 
             info = "La base de datos está vacía, no se pueden hacer los gráficos"
 
@@ -27,88 +55,10 @@
         }
         else{
 
-            // @ts-ignore
-            const years = affiliates.map(affiliates => affiliates.year).sort(function(a, b) {return a - b;});
+            console.log(affiliation)
 
-            const year = years[0]
-            // @ts-ignore
-            const sevilla = affiliates.filter((affiliate)=> affiliate.province==='sevilla').map(affiliate=> affiliate.ss_affiliation).sort(function(a, b) {return a.year - b.year;});
-            // @ts-ignore
-            const huelva = affiliates.filter((affiliate)=> affiliate.province==='huelva').map(affiliate=> affiliate.ss_affiliation).sort(function(a, b) {return a.year - b.year;});
-            // @ts-ignore
-            const almeria = affiliates.filter((affiliate)=> affiliate.province==='almeria').map(affiliate=> affiliate.ss_affiliation).sort(function(a, b) {return a.year - b.year;});
-            // @ts-ignore
-            const jaen = affiliates.filter((affiliate)=> affiliate.province==='jaen').map(affiliate=> affiliate.ss_affiliation).sort(function(a, b) {return a.year - b.year;});
-            // @ts-ignore
-            const granada = affiliates.filter((affiliate)=> affiliate.province==='granada').map(affiliate=> affiliate.ss_affiliation).sort(function(a, b) {return a.year - b.year;});
-            // @ts-ignore
-            const malaga = affiliates.filter((affiliate)=> affiliate.province==='malaga').map(affiliate=> affiliate.ss_affiliation).sort(function(a, b) {return a.year - b.year;});
-            // @ts-ignore
-            const cordoba = affiliates.filter((affiliate)=> affiliate.province==='cordoba').map(affiliate=> affiliate.ss_affiliation).sort(function(a, b) {return a.year - b.year;});
-            // @ts-ignore
-            const cadiz = affiliates.filter((affiliate)=> affiliate.province==='cadiz').map(affiliate=> affiliate.ss_affiliation).sort(function(a, b) {return a.year - b.year;});
-
-            // @ts-ignore
-            const chart = Highcharts.chart('container', {
-                chart: {
-                    type: 'area'
-                },
-                title: {
-                    text: 'Afiliados en Andalucia',
-                    align: 'left'
-                },
-                yAxis: {
-                    title: {
-                        useHTML: true,
-                        text: 'Afiliados a la seguridad social'
-                    }
-                },
-                tooltip: {
-                    shared: true,
-                    headerFormat: '<span style="font-size:12px"><b>{point.key}</b></span><br>'
-                },
-                plotOptions: {
-                    series: {
-                        pointStart: year
-                    },
-                    area: {
-                        stacking: 'normal',
-                        lineColor: '#666666',
-                        lineWidth: 1,
-                        marker: {
-                            lineWidth: 1,
-                            lineColor: '#666666'
-                        }
-                    }
-                },
-                series: [{
-                    name: 'Sevilla',
-                    data: sevilla
-                }, {
-                    name: 'Huelva',
-                    data: huelva
-
-                }, {
-                    name: 'Almeria',
-                    data: almeria
-                }, {
-                    name: 'Jaen',
-                    data: jaen
-
-                }, {
-                    name: 'Granada',
-                    data: granada
-                },{
-                    name: 'Malaga',
-                    data: malaga
-                },{
-                    name: 'Cadiz',
-                    data: cadiz
-                },{
-                    name: 'Cordoba',
-                    data: cordoba
-                }]
-            });   
+            chart1();
+           
 
             
         }
@@ -127,7 +77,7 @@
 
     async function getAffiliation() {
 
-        let affiliates = [];
+        let list = [];
 
         const res = await fetch(API, {
             method: 'GET'
@@ -135,7 +85,7 @@
         try{
 
             const data = await res.json();
-            affiliates = data;
+            list = data;
             
         }catch(error){
 
@@ -144,12 +94,103 @@
         // @ts-ignore
         const status = await res.status;
 
-        return affiliates
+        return list
     }
 
+    async function chart1(){
 
+        let affiliates = await getAffiliation();
+        // @ts-ignore
+        const years = affiliates.map(affiliate => affiliate.year).sort(function(a, b) {return a - b;});
+
+        const year = years[0]
+        // @ts-ignore
+        const sevilla = affiliates.filter((affiliate)=> affiliate.province==='sevilla').sort(function(a, b) {return a.year - b.year;}).map(affiliate=> affiliate.ss_affiliation);
+        // @ts-ignore
+        const huelva = affiliates.filter((affiliate)=> affiliate.province==='huelva').sort(function(a, b) {return a.year - b.year;}).map(affiliate=> affiliate.ss_affiliation);
+        // @ts-ignore
+        const almeria = affiliates.filter((affiliate)=> affiliate.province==='almeria').sort(function(a, b) {return a.year - b.year;}).map(affiliate=> affiliate.ss_affiliation);
+        // @ts-ignore
+        const jaen = affiliates.filter((affiliate)=> affiliate.province==='jaen').sort(function(a, b) {return a.year - b.year;}).map(affiliate=> affiliate.ss_affiliation);
+        // @ts-ignore
+        const granada = affiliates.filter((affiliate)=> affiliate.province==='granada').sort(function(a, b) {return a.year - b.year;}).map(affiliate=> affiliate.ss_affiliation);
+        // @ts-ignore
+        const malaga = affiliates.filter((affiliate)=> affiliate.province==='malaga').sort(function(a, b) {return a.year - b.year;}).map(affiliate=> affiliate.ss_affiliation);
+        // @ts-ignore
+        const cordoba = affiliates.filter((affiliate)=> affiliate.province==='cordoba').sort(function(a, b) {return a.year - b.year;}).map(affiliate=> affiliate.ss_affiliation);
+        // @ts-ignore
+        const cadiz = affiliates.filter((affiliate)=> affiliate.province==='cadiz').sort(function(a, b) {return a.year - b.year;}).map(affiliate=> affiliate.ss_affiliation);
+        
+        // @ts-ignore
+        const chart = Highcharts.chart('container', {
+            chart: {
+                type: 'area'
+            },
+            title: {
+                text: 'Afiliados en Andalucia',
+                align: 'left'
+            },
+            yAxis: {
+                title: {
+                    useHTML: true,
+                    text: 'Afiliados a la seguridad social'
+                }
+            },
+            tooltip: {
+                shared: true,
+                headerFormat: '<span style="font-size:12px"><b>{point.key}</b></span><br>'
+            },
+            plotOptions: {
+                series: {
+                    pointStart: year
+                },
+                area: {
+                    stacking: 'normal',
+                    lineColor: '#666666',
+                    lineWidth: 1,
+                    marker: {
+                        lineWidth: 1,
+                        lineColor: '#666666'
+                    }
+                }
+            },
+            series: [{
+                name: 'Sevilla',
+                data: sevilla
+            }, {
+                name: 'Huelva',
+                data: huelva
+
+            }, {
+                name: 'Almeria',
+                data: almeria
+            }, {
+                name: 'Jaen',
+                data: jaen
+
+            }, {
+                name: 'Granada',
+                data: granada
+            },{
+                name: 'Malaga',
+                data: malaga
+            },{
+                name: 'Cadiz',
+                data: cadiz
+            },{
+                name: 'Cordoba',
+                data: cordoba
+            }]
+        });   
+    }
 </script>
 <main>
+    <link href="../../node_modules/c3/c3.css" rel="stylesheet">
+
+    <script src="../../node_modules/d3/dist/d3.min.js" charset="utf-8"></script>
+    <script src="../../node_modules/c3/c3.min.js"></script>
+
+
     <Container>
         <Row>
             <Col sm={{ size: 'auto', offset: 2 }}><h1> Gráfica Seguridad Social en Andalucía</h1></Col>
@@ -162,7 +203,41 @@
     {#if info != ""}
         <Row><Col><Alert color="info" isOpen={v_info} toggle={() => (v_info = false)}>{info}</Alert></Col></Row>
     {/if}
-    <Row><div id="container" style="width:100%; height:400px;"></div></Row>
+        <Row><div id="container" style="width:100%; height:400px;"></div></Row>
+        <Row><div id="container2"></div></Row>
     </Container>
+
+    <script>
+
+        var chart = c3.generate({
+            bindto: document.getElementById('container2'),
+            data: {
+                columns: [
+                    ['data1', 609250, 602963, 617140, 626250, 641183, 662450, 683295, 696221, 689844, 714337, 729400],
+                    ['data2', 175997, 180755, 184601, 184162, 189533, 199026, 198047, 199047, 179297, 181757, 75484]
+                ]
+            },axes: {
+                data2: 'y2'
+            },
+            types: {
+                data2: 'bar' 
+            },
+            axis: {
+                y: {
+                    label: {
+                        text: 'Y Label',
+                        position: 'outer-middle'
+                    }
+                },
+                y2: {
+                    show: true,
+                    label: {
+                        text: 'Y2 Label',
+                        position: 'outer-middle'
+                    }
+                }
+            }
+        });
+    </script>
 </main>
 <style></style>
