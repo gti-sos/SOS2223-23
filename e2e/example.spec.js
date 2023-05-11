83547382eae345dcb933b59c0a0ad20730b78794
ricardovrodri <93518567+ricardovrodri@users.noreply.github.com>
2023-05-11 16:50:54 +0200

    tests de playwright pruebas final

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -2,17 +2,27 @@
 import  { test, expect } from '@playwright/test';
 
 test('Ricardo Test', async ({ page }) => {
+
   await page.goto('localhost:12345/api/v2/ss-affiliates/loadInitialData');
 
   await page.goto('localhost:12345/ss-affiliates');
 
   await page.waitForLoadState('load');
 
-  const EditButtons = await page.$$('button[name*="Editar"]');
-  console.log(EditButtons)
-  const numEditButtons =  EditButtons.length;
-  console.log(numEditButtons);
-  expect(numEditButtons).toBeGreaterThan(8);
+  await page.waitForTimeout(1000);
+
+  const links = await page.$$('a');
+
+  const linkTexts = [];
+
+  for (const link of links) {
+    const linkText = await link.textContent(); // obtener el texto del enlace
+    linkTexts.push(linkText); // añadir el texto a la lista
+  }
+
+  const numEditLink = linkTexts.filter((linkText) => linkText === 'Editar').length;
+  
+  expect(numEditLink).toBeGreaterThan(8);
 });
 
 test('get started link', async ({ page }) => {
