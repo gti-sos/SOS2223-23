24f013cf60fe11996cdde7449723d9f9692173e0
agujimcas <agujimcas@alum.us.es>
2023-05-18 09:44:09 +0200

    entrega final

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -75,7 +75,7 @@
         <ul>
             <li><a href="https://youtu.be/Knl9IHW5sDM">Vídeo ss-affiliates</a> (Realizado por Ricardo Vaz Rodriguez)</li>
             <li><a href="https://youtu.be/SzrGGParchU">Vídeo density-population</a> (Realizado por Pablo Antonio Perez Ortega)</li>
-            <li><a href="/hired-people">Vídeo hired-people</a> (Realizado por Agustín Manuel Jiménez Casas)</li>
+            <li><a href="/https://youtu.be/5FE9Sv3-eTc">Vídeo hired-people</a> (Realizado por Agustín Manuel Jiménez Casas)</li>
         </ul>
     </ul>
 </main>
